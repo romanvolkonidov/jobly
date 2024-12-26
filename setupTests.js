9e99df5e34c8ce3dd8fa3610e858f0698d03d679
romanvolkonidov <romanvolkonidov@gmail.com>
2024-12-26 04:57:01 +0300

    g

--- setupTests.js
+++
@@ -1,7 +0,0 @@
-// setupTests.js
-
-// Configure Testing Library (optional)
-import '@testing-library/jest-dom'; // Add custom matchers for assertions like toBeInTheDocument
-
-// Set up global mocks or configurations
-jest.setTimeout(30000); // Example: Increase test timeout globally
